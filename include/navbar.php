44eb3649725d45d5522e59939eabbeff646fbd64
Yuri Hartmann <yurihartmann0607@gmail.com>
2019-08-14 15:15:15 -0300

    Padrao para desenvolvimento aplicado

---
+++ include/navbar.php
@@ -0,0 +1,58 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand pr-3" href="#"><strong>FIDELIZE</strong></a>
+
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Inicio<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item active d-none d-lg-block">
+                <span class="nav-link font-weight-light">|</span>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Pricing</a>
+            </li>
+            <li class="nav-item active  d-none d-lg-block">
+                <span class="nav-link font-weight-light">|</span>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Preços</a>
+            </li>
+            <li class="nav-item active  d-none d-lg-block">
+                <span class="nav-link font-weight-light">|</span>
+            </li>
+            <div class="">
+                <li class="nav-item dropdown" onmouseover="">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
+                       aria-haspopup="true" aria-expanded="false">
+                        Informações
+                    </a>
+                    <div class="dropdown-menu rounded-0 border-top border-right-0 border-right-0 border-left-0 border-bottom-0 border-dark"
+                         id="lista" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item" href="#">Como funciona</a>
+                        <a class="dropdown-item" href="#">Quem somos</a>
+                    </div>
+                </li>
+            </div>
+        </ul>
+    </div>
+    <div class="float-right mr-3 d-none d-lg-block">
+        <a href="#" id="a"><span class="pr-2">Login</span></a>
+        <button type="button" class="btn btn-primary">Quero Fidelizar</button>
+    </div>
+
+    <div class="container-flex d-block  d-lg-none">
+        <div class="row m-0  fixed-bottom px-2 py-3">
+            <div class="col m-0">
+                <button type="button" class="btn btn-primary btn-block">Login</button>
+            </div>
+            <div class="col m-0">
+                <button type="button" class="btn btn-primary btn-block">Quero Fidelizar</button>
+            </div>
+        </div>
+    </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+include "include/header.php";
+include "include/navbar.php"
+
+?>
+
+
+
+<div class="container">
+    <h1 class="text-center">INDEX</h1>
+</div>
