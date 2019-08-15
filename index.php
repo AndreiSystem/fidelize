5c214938ed19ea3bfc17f951e572de1e66c28ed7
Andrei Gustavo <andrei.gustavo96@gmail.com>
2019-08-15 09:39:33 -0300

    atualizaçoes

--- index.php
+++ index.php
@@ -1,12 +1,25 @@
 <?php
 
 include "include/header.php";
-include "include/navbar.php"
+include "include/navbar.php";
+
 
 ?>
 
+<section>
+	<div class="container-fluid" id="mesa">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6 col-sm-12 text-center">
+					<img src="media/images/FIDELIZE_BRANCO.png" class="my-5">
+				</div>
+				<div class="col-md-6 col-sm-12">
+					<p class="display font-weight-light text-justify mt-5 text-white" style="font-size: 20px">
+					Nossa plataforma CRM é focada em processos de pós vendas e relacionamento com o cliente. Nós temos uma missão: "Elevar o padrão de qualidade do atendimento ao cliente no Brasil." E vamos fazer isso no seu negócio junto com você.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
 
-
-<div class="container">
-    <h1 class="text-center">INDEX</h1>
-</div>
+<?php include "include/footer.php" ?>
